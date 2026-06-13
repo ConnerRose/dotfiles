d34f0707a47346fbfa79b38aaa7a3abb168e1ee0
Conner Rose <conner.n.rose@gmail.com>
2026-06-13 18:39:00 +0800

    Current

--- .config/nvim/ftplugin/typst.lua
+++ .config/nvim/ftplugin/typst.lua
@@ -2,6 +2,14 @@ local map = vim.keymap.set
 map("n", "<leader>ll", ":TypstPreview<CR>", { buffer = 0 })
 map({ "n", "x", "v" }, "j", "gj", { buffer = 0 })
 map({ "n", "x", "v" }, "k", "gk", { buffer = 0 })
+map("n", "<leader>fm", function()
+  local file = vim.fn.expand("%:p")
+  if file == "" then
+    return
+  end
+  vim.cmd("silent !typstyle -i " .. vim.fn.shellescape(file))
+  vim.cmd("edit!")
+end, { buffer = true, desc = "Format Typst file with typstyle" })
 
 vim.opt_local.textwidth = 80
 vim.opt_local.wrapmargin = 0
